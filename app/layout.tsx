394e3af3c868d6398c1ac091099a59f1861163c4
Jorge Moreira <moreira.ajn@gmail.com>
2023-02-22 00:11:01 +0000

    astro-blog added

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image"
+import Link from "next/link";
+import "../src/styles/globals.css"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  const header = (
+    <header>
+      <div className="text-center bg-slate-800 p-8 mt-6 my-6 rounded-md">
+      <Image src="/logo.png" width={40} height={40} className="mx-auto"/>
+      <Link href="/">
+        <h1 className="text-2xl text-white font-bold mt-4">Astro Blog</h1>
+        </Link>
+      <p className="text-slate-300">Welcome to Astro Blog</p>
+    </div>
+    </header>
+  );
+
+  const footer = (
+    <footer>
+      <div className="border-t border-slate-500 mt-12 py-6 text-center text-slate-400">
+      <h3>Developed by Jorge Moreira</h3>
+    </div>
+    </footer>
+  );
+
+
+
+  return (
+    <html>
+      <head />
+      <body>
+        <div className="mx-auto max-w-2xl px-6">
+          {header}
+          {children}
+          {footer}
+        </div>
+      </body>
+    </html>
+  )
+}
